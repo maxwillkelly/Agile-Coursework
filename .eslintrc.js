9578ad59687c1cc807e9aa620ba921fe244ad460
Max Kelly <maxwill.kelly@gmail.com>
2021-01-23 00:55:29 +0000

    Formats/checks code on save + commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
     extends: [
         'eslint:recommended',
         'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
         'plugin:jsx-a11y/recommended',
         'plugin:prettier/recommended' // Make this the last element so prettier config overrides other formatting rules
     ],
