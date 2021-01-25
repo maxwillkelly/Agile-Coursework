f1fa1c5fe753fec969daba7ad956f30cc28e8b19
Max Kelly <maxwill.kelly@gmail.com>
2021-01-25 15:56:24 +0000

    Working login system

--- web/pages/api/login.js
+++ web/pages/api/login.js
@@ -1,7 +1,12 @@
-// import axois from 'axois';
+import axios from 'axios';
 
-const login = (data) => {
-    console.log(data);
+const login = async (data) => {
+    try {
+        const response = await axios.post('http://188.34.206.120:4000/login', data);
+        console.log(response);
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 export default login;
