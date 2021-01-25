1f4bdfd1a8eac6400ad86ae1330906cd36eac21a
Vincent Lee <vlee@vlee.me.uk>
2021-01-25 00:47:49 +0000

    Create basic jest test suite for Apollo Server

---
+++ server/src/tests/example.test.js
@@ -0,0 +1,32 @@
+const { createTestClient } = require('apollo-server-testing');
+const { gql } = require("apollo-server-express");
+const {constructTestServer} = require('./utils');
+
+const server = constructTestServer({})
+const { query, mutate } = createTestClient(server);
+
+it('primes query', async () => {
+    //Define the GraphQL query you want to use
+    const primes = gql`
+        query{
+            getPrimes
+        }
+    `
+
+    //Run Query against server
+    const { data: { getPrimes } } = await query({ query: primes })
+    expect(getPrimes).toEqual([2, 3, 5, 7, 11])
+})
+
+it('add Muation', async () => {
+    //Define the GraphQL query you want to use
+    const addNum = gql`
+        mutation{
+            addNumbers(values: [1,2,3])
+        }
+    `
+
+    //Run Query against server
+    const { data: { addNumbers } } = await mutate({ mutation: addNum })
+    expect(addNumbers).toEqual(6)
+})
---
+++ server/src/tests/utils.js
@@ -0,0 +1,13 @@
+const { ApolloServer } = require("apollo-server-express");
+const schema = require('../schema');
+
+function constructTestServer(context) {
+    const server = new ApolloServer({
+        schema: schema,
+        context: context
+    });
+
+    return server
+}
+
+module.exports.constructTestServer = constructTestServer;
