074ab58a611cb968b7831825d43c72ed8e83247c
Nick De Villiers <noodles524@gmail.com>
2021-01-26 11:08:24 +0000

    Started page setup for questionnaire creation. Fixed my awful spelling

---
+++ web/pages/questionnaire-creator.jsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+import { Container } from 'react-bootstrap';
+// import styles from '../styles/questionnaire-creator.module.scss';
+
+export default function QuestionairreCreatorPage() {
+    return (
+        <div>
+            <Head>
+                <title>Create a Questionairre</title>
+                <link rel="icon" href="favivon.ico" />
+            </Head>
+
+            <main>
+                <Container></Container>
+            </main>
+        </div>
+    );
+}
