6c146a46db11f1526702140bc04562b870cac231
Max Kelly <maxwill.kelly@gmail.com>
2021-01-27 21:22:38 +0000

    Attempts create questionnaire system

--- web/pages/questionnaires.jsx
+++ web/pages/questionnaires.jsx
@@ -1,28 +1,36 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { useQuery } from '@apollo/client';
-import { Button, ButtonGroup, Container, ListGroup } from 'react-bootstrap';
+import { useQuery, useMutation } from '@apollo/client';
+import { Button, Container, ListGroup } from 'react-bootstrap';
 import Navigation from '../components/Navigation';
 import { GET_QUESTIONNAIRES } from '../queries/questionnaire';
+import { CREATE_QUESTIONNAIRE } from '../mutations/questionnaire';
 import styles from '../styles/questionnaires.module.scss';
 
 const QuestionnairesPage = () => {
-    const router = useRouter();
+    // const router = useRouter();
+    const [createQuestionnaireMutation] = useMutation(CREATE_QUESTIONNAIRE);
     const getQuestionnaires = useQuery(GET_QUESTIONNAIRES);
+
+    const createQuestionnaire = () => {
+        const questionnaire = { title: '', description: '', studyID: '' };
+        createQuestionnaireMutation({
+            variables: { questionnaire }
+        }).then((data) => console.log(data));
+        // data.then(() => router.push(`/studies/questionnaire/${data.createQuestionaire.id}`));
+    };
 
     return (
         <>
             <Head>
-                <title>Questionnaires</title>
+                <title>Questionnaires</title>W
             </Head>
             <Navigation />
             <main>
                 <Container className="mt-3">
                     <h2 className="mx-3">All Questionnaires</h2>
                     <Questionnaires getQuestionnaires={getQuestionnaires} />
-                    <Button
-                        className="float-right mt-3"
-                        onClick={() => router.push('/studies/questionnaire/creator')}>
+                    <Button className="float-right mt-3" onClick={createQuestionnaire}>
                         Create Questionnaire
                     </Button>
                 </Container>
@@ -33,6 +41,7 @@ const QuestionnairesPage = () => {
 
 const Questionnaires = ({ getQuestionnaires }) => {
     const { loading, error, data } = getQuestionnaires;
+
     const router = useRouter();
 
     if (loading) return <p>Loading...</p>;
