ada7f6fc240db5e826c15734afe7666ea5d4bebc
Vincent Lee <vlee@vlee.me.uk>
2021-01-27 17:15:02 +0000

    edit question datatype

---
+++ server/src/func/questionType.js
@@ -0,0 +1,18 @@
+const idToQuestion = [
+    "radio",
+    "checkbox",
+    "shortAnswer",
+    "longAnswer"
+]
+
+const questionToID = {
+    radio: 1,
+    checkbox: 2,
+    shortAnswer: 3,
+    longAnswer: 4
+}
+
+module.exports = {
+    idToQuestion: idToQuestion,
+    questionToID:questionToID
+}
--- server/src/graphql-schemas/questionnaire.js
+++ server/src/graphql-schemas/questionnaire.js
@@ -13,7 +13,7 @@ const mongo = require('mongodb');
 const typeDefs = gql`
 
     input QuestionInput{
-        qType: Int!
+        qType: String!
         message: String!
         values: [String]!
     }
@@ -26,7 +26,7 @@ const typeDefs = gql`
 
    type Question{
         qID: ID
-        qType: Int
+        qType: String
         message: String
         values: [String]
    }
