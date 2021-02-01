b3f0f817ed12b07a690eabbee6a0b90d81234954
Vincent Lee <vlee@vlee.me.uk>
2021-02-01 14:51:36 +0000

    Comment for functions

--- server/src/func/study.js
+++ server/src/func/study.js
@@ -1,14 +1,22 @@
+/*
+Contains functions to retreave a study in form of the graphQL scheme
+*/
 const database = require('../database');
 const mongo = require('mongodb');
 const { IdError, PermissionsError } = require('../func/errors');
 const staffHelper = require('../func/staff')
 
+/**
+ * get the details of a study
+ * @param {mongo.ObjectID} studyID - ID of the study to return
+ */
 async function getStudy(studyID) {
     const StudyCollection = database.getDb().collection('study');
     const currStudy = await StudyCollection.findOne({ "_id": studyID })
     if (!currStudy) {
         throw new IdError("Invalid StudyID")
     }
+    // Builds the list of staff that are apart of the study to return
     staffReply = []
     try{
         for (let y in currStudy.staff) {
