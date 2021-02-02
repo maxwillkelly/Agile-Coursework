f65667b096bf6c300b81a3f272617e04c4e75cf0
Max Kelly <maxwill.kelly@gmail.com>
2021-02-02 00:05:17 +0000

    Working breadcrumbs system

--- web/components/MainBreadcrumb.jsx
+++ web/components/MainBreadcrumb.jsx
@@ -1,26 +1,34 @@
 import { useRouter } from 'next/router';
 import { Breadcrumb } from 'react-bootstrap';
+import lodash from 'lodash';
 
 const MainBreadcrumb = () => {
     const router = useRouter();
-    const segments = router.pathname === '/' ? [''] : router.pathname.split('/');
+    let segments = !router.asPath ? [''] : router.asPath.split('/');
     const activeSegment = segments[segments.length - 1];
 
+    // console.log(router);
+    // console.log(`segments: ${segments}`);
+    // console.log(`activeSegment: ${activeSegment}`);
+
     const hrefDictionary = {
-        '': 'Home',
-        admin: 'Admin',
-        login: 'Login',
-        studies: 'Studies',
-        'studies/[studyId]': 'StudyName',
-        'studies/[studyId]/questionnaire/[questionnaireId]/answer': 'Answer QuestionnaireName',
-        'studies/[studyId]/questionnaire/[questionnaireId]/edit': 'Edit QuestionnaireName'
+        '/admin': 'Admin',
+        '/login': 'Login',
+        '/studies': 'Studies',
+        [`/studies/${router.query.studyId}`]: 'StudyName',
+        [`/studies/${router.query.studyId}/questionnaire/${router.query.questionnaireId}/answer`]: 'Answer QuestionnaireName',
+        [`/studies/${router.query.studyId}/questionnaire/${router.query.questionnaireId}/edit`]: 'Edit QuestionnaireName'
     };
 
     return (
         <Breadcrumb>
-            {segments.map((href, i) => {
+            {segments.map((snippet, i) => {
+                let href = lodash.take(segments, i + 1).join('/');
                 const linkName = hrefDictionary[href];
-                href = !href ? '/' : href;
+
+                // console.log(`href: ${href}`);
+                // console.log(`linkName: ${linkName}`);
+
                 return (
                     <MyBreadcrumbItem href={href} activeSegment={activeSegment} key={i}>
                         {linkName ? linkName : href}
@@ -34,7 +42,12 @@ const MainBreadcrumb = () => {
 const MyBreadcrumbItem = ({ children, href }) => {
     const router = useRouter();
     const path = router.pathname === '/' ? `/${router.pathname}` : router.pathname;
-    if (href === '/' || href === 'questionnaire') return null;
+    if (
+        href === '' ||
+        href.endsWith('questionnaire') ||
+        href.endsWith(router.query.questionnaireId)
+    )
+        return null;
 
     return (
         <Breadcrumb.Item onClick={() => router.push(href)} active={path === `/${href}`}>
