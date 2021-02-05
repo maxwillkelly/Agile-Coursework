b477cfebef26d76959a9aff04d6a3b6947e252e1
Brandon <B.Y.Clark@dundee.ac.uk>
2021-02-05 11:44:08 +0000

    Added file saving functionality

--- web/components/ethics/ethicsPacker.tsx
+++ web/components/ethics/ethicsPacker.tsx
@@ -1,15 +1,20 @@
-import { saveAs } from "file-saver";
+import * as saveAs from "file-saver";
+import * as fs from "fs";
 import { Packer, Document } from "docx";
-
-
 
 export function generateDoc(doc: Document): void {
     console.log("Not yet");
+    console.log('doc', doc);
     Packer.toBlob(doc).then(blob => {
-        console.log(blob);
+        console.log('blob', blob);
         saveAs(blob, "Output.docx");
         console.log("Document created successfully");
-    }),
+    }).catch(err => {
+        console.log("err", err);
+    })
+    // Packer.toBuffer(doc).then((buffer) => {
+    //     fs.writeFileSync("My Document.docx", buffer);
+    // });
     console.log("Yes")
 }
 
